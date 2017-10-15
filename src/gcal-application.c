22f2b6fa109d01dbe9385c90a8e00efef4b39a4f
Florian Brosch <flo.brosch@gmail.com>
2017-10-15 21:58:57 +0200

    weather: Re-use old weather predictions on errors

--- src/gcal-application.c
+++ src/gcal-application.c
@@ -37,7 +37,8 @@
 
 #define CSS_FILE "/org/gnome/calendar/gtk-styles.css"
 
-#define WEATHER_CHECK_INTERVAL 7200 /* seconds */
+#define WEATHER_CHECK_INTERVAL 2*60*60  /* seconds */
+#define WEATHER_VALID_TIMESPAN 24*60*60 /* seconds */
 #define FORECAST_MAX_DAYS      3
 
 struct _GcalApplication
@@ -403,7 +404,8 @@ gcal_application_init (GcalApplication *self)
   g_signal_connect_swapped (self->manager, "source-changed", G_CALLBACK (process_sources), self);
   self->weather_service = gcal_weather_service_new (NULL, /* in prep. for configurable time zones */
                                                     FORECAST_MAX_DAYS,
-                                                    WEATHER_CHECK_INTERVAL);
+                                                    WEATHER_CHECK_INTERVAL,
+                                                    WEATHER_VALID_TIMESPAN);
   self->search_provider = gcal_shell_search_provider_new ();
   gcal_shell_search_provider_connect (self->search_provider, self->manager);
 }
