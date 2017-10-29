c23d7c3814182da41289f2c4c09365566bc119e4
Florian Brosch <flo.brosch@gmail.com>
2017-10-29 16:38:00 +0100

    month-view: Show weather reports.
    This commit triggers two existing issues:
     1. window size calculation
        (this breaks month-cells)
     2. wrong reference counts in views
        (I use :destroy to avoid dangling pointers)

--- src/views/gcal-month-cell.c
+++ src/views/gcal-month-cell.c
@@ -31,7 +31,8 @@ struct _GcalMonthCell
   GDateTime          *date;
   guint               n_overflow;
 
-  GtkLabel           *day_label;
+  GtkLabel           *day_label; /* unowned */
+  GtkImage           *weather_icon;
   GtkWidget          *overflow_button;
   GtkWidget          *overflow_label;
   GtkWidget          *overlay;
@@ -422,6 +423,7 @@ gcal_month_cell_class_init (GcalMonthCellClass *klass)
   gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/calendar/month-cell.ui");
 
   gtk_widget_class_bind_template_child (widget_class, GcalMonthCell, day_label);
+  gtk_widget_class_bind_template_child (widget_class, GcalMonthCell, weather_icon);
   gtk_widget_class_bind_template_child (widget_class, GcalMonthCell, overflow_button);
   gtk_widget_class_bind_template_child (widget_class, GcalMonthCell, overflow_label);
   gtk_widget_class_bind_template_child (widget_class, GcalMonthCell, overlay);
@@ -478,6 +480,38 @@ gcal_month_cell_set_date (GcalMonthCell *self,
 
   gtk_label_set_text (self->day_label, text);
   update_style_flags (self);
+}
+
+/**
+ * gcal_month_cell_set_weather:
+ * @self: The #GcalMonthCell instance.
+ * @info: (nullable): The weather information to display.
+ *
+ * Sets the weather information to display for this day.
+ *
+ * Note that this function does not check dates nor
+ * manages weather information changes on its own.
+ */
+void
+gcal_month_cell_set_weather (GcalMonthCell   *self,
+                             GcalWeatherInfo *info)
+{
+  g_return_if_fail (GCAL_IS_MONTH_CELL (self));
+  g_return_if_fail (info == NULL || GCAL_IS_WEATHER_INFO (info));
+
+  if (info == NULL)
+    {
+      gtk_image_clear (self->weather_icon);
+    }
+  else
+    {
+      const gchar* icon_name; /* unowned */
+
+      icon_name = gcal_weather_info_get_icon_name (info);
+      gtk_image_set_from_icon_name (self->weather_icon, icon_name, GTK_ICON_SIZE_SMALL_TOOLBAR);
+    }
+
+  gtk_widget_show (GTK_WIDGET (self->weather_icon));
 }
 
 gboolean
